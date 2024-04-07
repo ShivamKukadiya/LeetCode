360519114d2925f8412515a9303ee940677977f5
ShivamKukadiya <145829872+ShivamKukadiya@users.noreply.github.com>
2024-04-07 13:47:26 +0530

    Time: 19 ms (24.53%) | Memory: 20 MB (57.47%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -9,12 +9,12 @@ public:
         }
         for(int i = 1; i < n;  i++){
             if(r[i] > r[i-1] && c[i] <= c[i-1]){
-                c[i] =c[i-1]+1;
+                c[i] = c[i-1]+1;
             }
         }
         for(int i=n-2; i>=0; i--){
             if(r[i] > r[i+1] && c[i] <=c[i+1]){
-                c[i]=c[i+1]+1;
+                c[i] = c[i+1]+1;
             }
         }
         for(int i=0;i<n; i++){
