bb87f6b1170dafc4741ece2b7d4f86a44f82cf46
ShivamKukadiya <145829872+ShivamKukadiya@users.noreply.github.com>
2024-04-11 15:39:45 +0530

    Time: 21 ms (40.43%) | Memory: 16.7 MB (85.61%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -8,62 +8,40 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
-// class Node{
-//     public:
-//     ListNode* node;
-//     int row;
-
-//     Node(ListNode* node, int row){
-//         this->node = node;
-//         this->row = row;
-//     }
-// };
-
-class cmp{
+ class cmp{
     public:
     bool operator() (ListNode* a, ListNode* b){
         return a->val > b->val;
     }
 };
-
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        // ios_base::sync_with_stdio(false);
-        // cin.tie(NULL);
+        ListNode* head = nullptr;
+        ListNode* teil = nullptr;
+        priority_queue<ListNode*, vector<ListNode*>,cmp> mini;
 
-        ListNode* head = NULL;
-        ListNode* tail = NULL;
-        priority_queue<ListNode*, vector<ListNode*>, cmp> mini;
-
-        for(int i=0; i<lists.size(); i++){
-            if(lists[i] != NULL)
+        for (int i = 0; i < lists.size(); i++) {
+            if (lists[i] != nullptr) {
                 mini.push(lists[i]);
+            }
         }
 
-        while(!mini.empty()){
-            ListNode* minNode = mini.top();
-            
-            //add miniNode to ans List
-            if(head == NULL){
-                head = minNode;
-                tail = minNode;
+        while (!mini.empty()) {
+            ListNode* tr = mini.top();
+            if (head == nullptr) {
+                head = tr;
+                teil = tr;
+            } else {
+                teil->next = tr;
+                teil = tr;
             }
-
-            else{
-                tail -> next = minNode;
-                tail = minNode;
-            }
-
             mini.pop();
-        
-            if(minNode->next != NULL){
-                //insert next node of minNode List
-                mini.push(minNode->next);
+
+            if (tr->next != nullptr){
+                mini.push(tr->next);
             }
         }
-        
         return head;
     }
 };
