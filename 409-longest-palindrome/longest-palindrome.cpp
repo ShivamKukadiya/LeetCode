c6483a3113422f081290546c56a311ea873fa162
ShivamKukadiya <145829872+ShivamKukadiya@users.noreply.github.com>
2024-06-04 11:53:27 +0530

    Time: 6 ms (16.33%) | Memory: 8.9 MB (48.94%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        map<char, int> mpp;
+        int ans = 0;
+        int temp = 0;
+        for (char i : s) {
+            mpp[i]++;
+        }
+        bool flag = true;
+        for (auto i : mpp) {
+            if (i.second & 1) {
+                if (flag) {
+                    ans += i.second;
+                    flag = false;
+                } else {
+                    ans += i.second - 1;
+                }
+            } else {
+                ans += i.second;
+            }
+        }
+        return ans + temp;
+    }
+};
