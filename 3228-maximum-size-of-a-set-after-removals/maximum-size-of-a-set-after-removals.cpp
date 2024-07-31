7ef8fc4e339a8350740d5e1958c7238f34f14ccd
ShivamKukadiya <145829872+ShivamKukadiya@users.noreply.github.com>
2024-07-31 10:09:02 +0530

    Time: 218 ms (45.87%) | Memory: 145.1 MB (35.96%) - LeetSync

--- 3228-maximum-size-of-a-set-after-removals/maximum-size-of-a-set-after-removals.cpp
+++ 3228-maximum-size-of-a-set-after-removals/maximum-size-of-a-set-after-removals.cpp
@@ -1,27 +1,36 @@
 class Solution {
 public:
     int maximumSetSize(vector<int>& nums1, vector<int>& nums2) {
-        unordered_set<int> s;
-        int n1=nums1.size();
-        int n2=nums2.size();
-        for(int i=0;i<n1;i++){
+        int ans;
+        unordered_set<int>s;
+
+        int n = nums1.size();
+        int m = nums2.size();
+    
+        for (int i = 0; i < n; i++) {
             s.insert(nums1[i]);
         }
-        int use1=s.size();
+        int s1 = s.size();
         s.clear();
-        for(int i=0;i<n2;i++){
+
+        for (int i = 0; i < m; i++) {
             s.insert(nums2[i]);
         }
-        int use2=s.size();
+        int s2 = s.size();
         s.clear();
-        for(int i=0;i<n1;i++){
+
+        for (int i = 0; i < n; i++) {
             s.insert(nums1[i]);
         }
-        for(int i=0;i<n2;i++){
+
+        for (int i = 0; i < m; i++) {
             s.insert(nums2[i]);
         }
-        int use3=s.size();
-        return min(use3,min(n1/2,use1)+min(n2/2,use2));
-        
+
+        int s3 = s.size();
+        s.clear();
+
+         return min(s3, min(n / 2 , s1) + min(m / 2 , s2));
+        // return ans;
     }
 };
