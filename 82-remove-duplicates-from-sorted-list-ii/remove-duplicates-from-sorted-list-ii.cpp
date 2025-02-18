6895fac1169d1f6574deffaa9dc969171fc02b72
ShivamKukadiya <145829872+ShivamKukadiya@users.noreply.github.com>
2025-02-18 11:17:31 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (17.62%) - LeetSync

--- 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.cpp
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.cpp
@@ -1,31 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    int arr[202];
-    ListNode* calc(ListNode* curr,ListNode* pre)
-    {
-        if(curr==nullptr)return nullptr;    //base case
-        if(arr[curr->val +100]>1)           //deletion check
-        {
-            if(pre==nullptr)                //head node deletion
-            {
-                ListNode* tmp=calc(curr->next,nullptr);
-                delete curr;
-                return tmp;
-            }
-            pre->next=curr->next;           //middle node deletion
-            delete curr;
-            return calc(pre->next,pre);
-        }
-        calc(curr->next,curr);              //skipping deletion
-        return curr;
-    }
-    //----------------------------------------------------------------------
     ListNode* deleteDuplicates(ListNode* head) {
-        memset(arr,0,sizeof(arr));
-        for(ListNode* tmp=head; tmp!=nullptr;tmp=tmp->next)
-        {
-            arr[tmp->val+100]++;            //+100 used to fit negative numbers in array
+        if (!head || !head->next) return head;  
+        
+        ListNode* dummy = new ListNode(0, head);  
+        ListNode* prev = dummy;  
+        
+        while (head) {
+            
+            if (head->next && head->val == head->next->val) {
+                
+                while (head->next && head->val == head->next->val) {
+                    head = head->next;
+                }
+                prev->next = head->next;  
+            } else {
+                prev = prev->next;  
+            }
+            head = head->next;  
         }
-        return calc(head,nullptr);
+        
+        return dummy->next;  
     }
 };
